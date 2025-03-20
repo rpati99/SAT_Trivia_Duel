c5dab096108a490f6c1589806906f35667277670
Rachit Prajapati <rdprajapati@icloud.com>
2025-03-20 12:11:06 -0700

    Initial Commit

---
+++ BrainBlast_Submission/BrainBlast_SubmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainBlast_SubmissionApp.swift
+//  BrainBlast_Submission
+//
+//  Created by Rachit Prajapati on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct BrainBlast_SubmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainBlast_Submission/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrainBlast_Submission
+//
+//  Created by Rachit Prajapati on 3/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
